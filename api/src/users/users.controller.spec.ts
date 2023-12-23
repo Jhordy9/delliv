2fa66b82d71785d62f66621c9b8b43a3b05c5e53
jhordy <jhooordy@gmail.com>
2023-12-22 23:09:08 -0300

    test(api): users service and controller

--- api/src/users/users.controller.spec.ts
+++ api/src/users/users.controller.spec.ts
@@ -1,18 +1,66 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import * as request from 'supertest';
 
 describe('UsersController', () => {
-  let controller: UsersController;
+  let app: INestApplication;
+  let usersService: UsersService;
 
   beforeEach(async () => {
+    const mockUsersService = {
+      register: jest.fn().mockImplementation((email, password, name) => {
+        return { id: 'some-unique-id', email, name };
+      }),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: mockUsersService,
+        },
+      ],
     }).compile();
 
-    controller = module.get<UsersController>(UsersController);
+    app = module.createNestApplication();
+    await app.init();
+
+    usersService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(UsersController).toBeDefined();
+  });
+
+  it('should register a user and return user data', async () => {
+    const registerDto = {
+      email: 'newuser@example.com',
+      password: 'strongpassword',
+      name: 'New User',
+    };
+
+    const response = await request(app.getHttpServer())
+      .post('/users/register')
+      .send(registerDto)
+      .expect(HttpStatus.CREATED);
+
+    expect(response.body).toEqual({
+      id: expect.any(String),
+      email: 'newuser@example.com',
+      name: 'New User',
+    });
+
+    expect(usersService.register).toHaveBeenCalledWith(
+      registerDto.email,
+      registerDto.password,
+      registerDto.name,
+    );
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
--- api/src/users/users.service.spec.ts
+++ api/src/users/users.service.spec.ts
@@ -1,18 +1,94 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { PrismaService } from 'src/prisma.service';
+import { ConflictException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+
+const mockUser = {
+  email: 'user@example.com',
+  password: 'hashedpassword',
+  name: 'Test User',
+};
+
+jest.mock('bcrypt', () => ({
+  hashSync: jest.fn().mockReturnValue('hashedpassword'),
+}));
 
 describe('UsersService', () => {
   let service: UsersService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: PrismaService,
+          useValue: {
+            user: {
+              findUnique: jest.fn().mockResolvedValue(null), // Assuming the user does not exist initially
+              create: jest.fn().mockResolvedValue(mockUser),
+            },
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findOne', () => {
+    it('should find a user by email', async () => {
+      prismaService.user.findUnique = jest.fn().mockResolvedValueOnce(mockUser);
+      const result = await service.findOne('user@example.com');
+      expect(result).toEqual(mockUser);
+      expect(prismaService.user.findUnique).toHaveBeenCalledWith({
+        where: { email: 'user@example.com' },
+      });
+    });
+  });
+
+  describe('register', () => {
+    it('should register a new user', async () => {
+      const newUser = {
+        email: 'newuser@example.com',
+        password: 'newpassword',
+        name: 'New User',
+      };
+      prismaService.user.create = jest.fn().mockResolvedValueOnce(newUser);
+
+      const result = await service.register(
+        newUser.email,
+        newUser.password,
+        newUser.name,
+      );
+
+      expect(bcrypt.hashSync).toHaveBeenCalledWith(newUser.password, 10);
+      expect(prismaService.user.create).toHaveBeenCalledWith({
+        data: {
+          email: newUser.email,
+          password: expect.any(String),
+          name: newUser.name,
+        },
+        select: {
+          email: true,
+          name: true,
+        },
+      });
+      expect(result).toEqual(newUser);
+    });
+
+    it('should throw a ConflictException if user already exists', async () => {
+      prismaService.user.findUnique = jest.fn().mockResolvedValueOnce(mockUser);
+
+      await expect(
+        service.register(mockUser.email, 'password', mockUser.name),
+      ).rejects.toThrow(ConflictException);
+    });
   });
 });
