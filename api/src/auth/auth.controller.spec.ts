4b5c9887d4bad6a29b780c688e92f7bc1a6b2968
jhordy <jhooordy@gmail.com>
2023-12-22 22:40:13 -0300

    test(api): orders service and controller

--- api/src/auth/auth.controller.spec.ts
+++ api/src/auth/auth.controller.spec.ts
@@ -1,20 +1,55 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Test, TestingModule } from '@nestjs/testing';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import * as request from 'supertest';
 
 describe('AuthController', () => {
-  let controller: AuthController;
+  let app: INestApplication;
+  let authService: AuthService;
 
   beforeEach(async () => {
+    // Mock AuthService
+    const mockAuthService = {
+      signIn: jest.fn().mockImplementation((email, password) => {
+        return { access_token: 'mockJwtToken' };
+      }),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+      ],
     }).compile();
 
-    controller = module.get<AuthController>(AuthController);
+    app = module.createNestApplication();
+    await app.init();
+
+    authService = module.get<AuthService>(AuthService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should return an access token when login is successful', async () => {
+    const signInDto = { email: 'user@example.com', password: 'password' };
+
+    const response = await request(app.getHttpServer())
+      .post('/auth/login')
+      .send(signInDto)
+      .expect(HttpStatus.OK);
+
+    expect(response.body).toEqual({ access_token: 'mockJwtToken' });
+
+    expect(authService.signIn).toHaveBeenCalledWith(
+      signInDto.email,
+      signInDto.password,
+    );
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
--- api/src/auth/auth.service.spec.ts
+++ api/src/auth/auth.service.spec.ts
@@ -1,18 +1,98 @@
-import { Test, TestingModule } from '@nestjs/testing';
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { AuthService } from './auth.service';
+import { UsersService } from '../users/users.service';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { jwtConstants } from './constants';
+import { DeepMockProxy } from 'jest-mock-extended';
+import { UnauthorizedException } from '@nestjs/common';
+import { compareSync } from 'bcrypt';
+
+const mockUser = {
+  id: '1',
+  email: 'user@example.com',
+  password: 'password',
+  name: 'Test User',
+  created_at: new Date(),
+  updated_at: new Date(),
+};
+
+jest.mock('bcrypt', () => {
+  return {
+    compareSync: jest.fn(() => true),
+    signAsync: jest.fn().mockReturnValue('mockJwtToken'),
+  };
+});
+
+jest.mock('src/prisma.service', () => {
+  return {
+    PrismaService: jest.fn().mockImplementation(() => {
+      return {
+        user: {
+          findUnique: jest.fn().mockResolvedValue(mockUser),
+        },
+      };
+    }),
+  };
+});
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  let spyUserService: UsersService;
+  let spyJwtService: JwtService;
+  let spyPrismaService: DeepMockProxy<PrismaService>;
 
-  beforeEach(async () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      imports: [
+        JwtModule.register({
+          secret: jwtConstants.secret,
+          signOptions: {
+            expiresIn: '30d',
+          },
+        }),
+      ],
+      providers: [AuthService, UsersService, PrismaService],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
+    spyUserService = module.get<UsersService>(UsersService);
+    spyJwtService = module.get<JwtService>(JwtService);
+    spyPrismaService = module.get(PrismaService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
+  });
+
+  describe('signIn', () => {
+    it('should successfully sign in a user', async () => {
+      const result = await authService.signIn('user@example.com', 'password');
+
+      expect(!!result).toBe(true);
+    });
+
+    it('should throw an UnauthorizedException if user is not found', async () => {
+      spyUserService.findOne = jest.fn().mockResolvedValue(null);
+
+      await expect(
+        authService.signIn('nonexistent@example.com', 'password'),
+      ).rejects.toThrow(UnauthorizedException);
+    });
+
+    it('should throw an UnauthorizedException if password is incorrect', async () => {
+      spyUserService.findOne = jest.fn().mockResolvedValue(mockUser);
+
+      jest.mocked(compareSync).mockReturnValue(false);
+
+      await expect(
+        authService.signIn('user@example.com', 'wrongpassword'),
+      ).rejects.toThrow(UnauthorizedException);
+    });
   });
 });
