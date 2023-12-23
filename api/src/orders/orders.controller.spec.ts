75eb4b3e718df92f946675d8221de03493183951
jhordy <jhooordy@gmail.com>
2023-12-22 23:02:42 -0300

    test(api): orders service and controller

--- api/src/orders/orders.controller.spec.ts
+++ api/src/orders/orders.controller.spec.ts
@@ -1,20 +1,96 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Test, TestingModule } from '@nestjs/testing';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { OrderStatus } from '@prisma/client';
+import * as request from 'supertest';
 
 describe('OrdersController', () => {
-  let controller: OrdersController;
+  let app: INestApplication;
+  let ordersService: OrdersService;
 
   beforeEach(async () => {
+    const mockOrdersService = {
+      getOrders: jest.fn().mockImplementation((params) => {
+        return {
+          data: [],
+          info: {
+            page: 1,
+            take: 10,
+            total_pages: 1,
+            total_items: 0,
+          },
+        };
+      }),
+      updateOrderStatus: jest.fn().mockImplementation((id, status) => ({
+        id,
+        status,
+      })),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [OrdersController],
-      providers: [OrdersService],
+      providers: [
+        {
+          provide: OrdersService,
+          useValue: mockOrdersService,
+        },
+      ],
     }).compile();
 
-    controller = module.get<OrdersController>(OrdersController);
+    app = module.createNestApplication();
+    await app.init();
+
+    ordersService = module.get<OrdersService>(OrdersService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(OrdersController).toBeDefined();
+  });
+
+  it('should return orders with pagination info', async () => {
+    const response = await request(app.getHttpServer())
+      .get('/orders')
+      .query({ status: OrderStatus.PENDING, page: '1', take: '10' })
+      .expect(HttpStatus.OK);
+
+    expect(response.body).toEqual({
+      data: [],
+      info: {
+        page: 1,
+        take: 10,
+        total_pages: 1,
+        total_items: 0,
+      },
+    });
+    expect(ordersService.getOrders).toHaveBeenCalledWith({
+      status: OrderStatus.PENDING,
+      page: '1',
+      take: '10',
+    });
+  });
+
+  it('should update an order status', async () => {
+    const orderId = '1';
+    const orderStatus = OrderStatus.SHIPPED;
+
+    const response = await request(app.getHttpServer())
+      .put(`/orders/${orderId}`)
+      .send({ status: orderStatus })
+      .expect(HttpStatus.OK);
+
+    expect(response.body).toEqual({
+      id: orderId,
+      status: orderStatus,
+    });
+    expect(ordersService.updateOrderStatus).toHaveBeenCalledWith(
+      orderId,
+      orderStatus,
+    );
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
--- api/src/orders/orders.service.spec.ts
+++ api/src/orders/orders.service.spec.ts
@@ -1,18 +1,105 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrdersService } from './orders.service';
+import { PrismaService } from 'src/prisma.service';
+import { OrderStatus } from '@prisma/client';
+import { NotFoundException } from '@nestjs/common';
+
+const mockPrismaService = {
+  order: {
+    findMany: jest.fn(),
+    count: jest.fn(),
+    findUnique: jest.fn(),
+    update: jest.fn(),
+  },
+};
 
 describe('OrdersService', () => {
   let service: OrdersService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OrdersService],
+      providers: [
+        OrdersService,
+        {
+          provide: PrismaService,
+          useValue: mockPrismaService,
+        },
+      ],
     }).compile();
 
     service = module.get<OrdersService>(OrdersService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getOrders', () => {
+    it('should return orders and pagination info', async () => {
+      const mockOrders = [
+        {
+          address: '123',
+          status: OrderStatus.PENDING,
+          user: { name: 'John Doe' },
+        },
+      ];
+      const mockCount = 1;
+      const params = {
+        status: OrderStatus.PENDING,
+        take: '10',
+        page: '1',
+        search: 'John',
+      };
+
+      mockPrismaService.order.findMany.mockResolvedValue(mockOrders);
+      mockPrismaService.order.count.mockResolvedValue(mockCount);
+
+      const result = await service.getOrders(params);
+
+      expect(prismaService.order.findMany).toHaveBeenCalledWith(
+        expect.anything(),
+      );
+      expect(prismaService.order.count).toHaveBeenCalledWith(expect.anything());
+      expect(result).toHaveProperty('data');
+      expect(result).toHaveProperty('info');
+    });
+  });
+
+  describe('updateOrderStatus', () => {
+    it('should update the order status', async () => {
+      const orderId = '1';
+      const status = OrderStatus.SHIPPED;
+      const mockOrder = { id: orderId, status: OrderStatus.PENDING };
+
+      mockPrismaService.order.findUnique.mockResolvedValue(mockOrder);
+      mockPrismaService.order.update.mockResolvedValue({
+        ...mockOrder,
+        status,
+      });
+
+      const result = await service.updateOrderStatus(orderId, status);
+
+      expect(prismaService.order.findUnique).toHaveBeenCalledWith({
+        where: { id: orderId },
+      });
+      expect(prismaService.order.update).toHaveBeenCalledWith({
+        where: { id: orderId },
+        data: { status },
+      });
+      expect(result.status).toBe(status);
+    });
+
+    it('should throw NotFoundException if order does not exist', async () => {
+      const orderId = '2';
+      const status = OrderStatus.SHIPPED;
+
+      mockPrismaService.order.findUnique.mockResolvedValue(null);
+
+      await expect(service.updateOrderStatus(orderId, status)).rejects.toThrow(
+        NotFoundException,
+      );
+    });
   });
 });
